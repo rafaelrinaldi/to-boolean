64332dacc40ab370e59e27b5091dce9d22d8076f
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-11 11:12:03 -0300

    Truthy values test suite.

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ var toBoolean = require('./');
 var test = require('tape');
 
 test('truthy values', function(t) {
-  t.deepEqual(toBoolean('y'), false, '"y" is "true"');
+  t.deepEqual(toBoolean('y'), true, '"y" is "true"');
+  t.deepEqual(toBoolean('yes'), true, '"yes" is "true"');
+  t.deepEqual(toBoolean('true'), true, '"true" is "true"');
   t.end();
 });
